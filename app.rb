d8ad69329d92912b66dea57f6971f6e87495016a
julianfoo <julianfssen@gmail.com>
2020-02-26 04:23:09 -0500

    Added delete and edit function

--- app.rb
+++ app.rb
@@ -1,18 +1,41 @@
 require './idea'
 
 class IdeaBoxApp < Sinatra::Base
+    set :method_override, true
+
     configure :development do
         register Sinatra::Reloader
     end
 
     get '/' do
-        erb :index
+        erb :index, locals: {ideas: Idea.all}
     end
 
     post '/' do
         idea = Idea.new(params['idea_title'], params['idea_description'])
         idea.save
-        "Create an idea"
+
+        redirect '/'
+    end
+
+    get '/:id/edit' do |id|
+        idea = Idea.find(id.to_i)
+        erb :edit, locals: {id: id, idea: idea}
+    end
+
+    put '/:id' do |id|
+        data = {
+            :title => params['idea_title'],
+            :description => params['idea_description']
+        }
+        Idea.update(id.to_i, data)
+        redirect '/'
+    end
+
+    delete '/:id' do |id|
+        Idea.delete(id.to_i)
+
+        redirect '/'
     end
 
     not_found do
